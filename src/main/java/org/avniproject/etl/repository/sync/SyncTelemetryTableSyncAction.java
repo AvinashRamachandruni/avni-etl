ea28bbc027720791e6d085ce25d182aaee480464
ak2502 <akanksha.feb25@gmail.com>
2023-07-21 18:44:23 +0530

    audit fields added to sync telemetry
    Signed-off-by: ak2502 <akanksha.feb25@gmail.com>

--- src/main/java/org/avniproject/etl/repository/sync/SyncTelemetryTableSyncAction.java
+++ src/main/java/org/avniproject/etl/repository/sync/SyncTelemetryTableSyncAction.java
@@ -4,15 +4,14 @@ import org.avniproject.etl.domain.OrgIdentityContextHolder;
 import org.avniproject.etl.domain.NullObject;
 import org.avniproject.etl.domain.metadata.SchemaMetadata;
 import org.avniproject.etl.domain.metadata.TableMetadata;
-import org.avniproject.etl.repository.AvniMetadataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.stringtemplate.v4.ST;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 
 import static org.avniproject.etl.repository.JdbcContextWrapper.runInOrgContext;
 import static org.avniproject.etl.repository.sql.SqlFile.readSqlFile;
@@ -20,15 +19,11 @@ import static org.avniproject.etl.repository.sql.SqlFile.readSqlFile;
 @Repository
 public class SyncTelemetryTableSyncAction implements EntitySyncAction {
     private final JdbcTemplate jdbcTemplate;
-    private final AvniMetadataRepository avniMetadataRepository;
     private static final String syncTelemetrySql = readSqlFile("syncTelemetry.sql.st");
 
-    private static final String deleteDuplicateSyncTelemetrySql = readSqlFile("deleteDuplicateSyncTelemetry.sql.st");
-
     @Autowired
-    public SyncTelemetryTableSyncAction(JdbcTemplate jdbcTemplate, AvniMetadataRepository metadataRepository) {
+    public SyncTelemetryTableSyncAction(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-        this.avniMetadataRepository = metadataRepository;
     }
 
     @Override
@@ -42,20 +37,21 @@ public class SyncTelemetryTableSyncAction implements EntitySyncAction {
             return;
         }
 
-                insertData(tableMetadata);
+                insertData(tableMetadata, lastSyncTime, dataSyncBoundaryTime);
     }
 
-    private void insertData(TableMetadata syncTelemetryTableMetadata) {
-        syncNewerRows(syncTelemetryTableMetadata);
+    private void insertData(TableMetadata syncTelemetryTableMetadata, Date lastSyncTime, Date dataSyncBoundaryTime) {
+        syncNewerRows(syncTelemetryTableMetadata,lastSyncTime, dataSyncBoundaryTime);
 
-        deleteDuplicateRows();
     }
 
-    private void syncNewerRows(TableMetadata syncTelemetryTableMetadata) {
+    private void syncNewerRows(TableMetadata syncTelemetryTableMetadata, Date lastSyncTime, Date dataSyncBoundaryTime) {
 
         ST template = new ST(syncTelemetrySql)
                 .add("schemaName", wrapInQuotes(OrgIdentityContextHolder.getDbSchema()))
-                .add("tableName", wrapInQuotes(syncTelemetryTableMetadata.getName()));
+                .add("tableName", wrapInQuotes(syncTelemetryTableMetadata.getName()))
+                .add("startTime", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").format(lastSyncTime))
+                .add("endTime", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").format(dataSyncBoundaryTime));
 
         String sql = template.render();
 
@@ -64,21 +60,6 @@ public class SyncTelemetryTableSyncAction implements EntitySyncAction {
             return NullObject.instance();
         }, jdbcTemplate);
     }
-
-    private void deleteDuplicateRows() {
-        String schema = OrgIdentityContextHolder.getDbSchema();
-        String sql = new ST(deleteDuplicateSyncTelemetrySql)
-                .add("schemaName", schema)
-                .render();
-        HashMap<String, Object> params = new HashMap<>();
-//        params.put("lastSyncTime", lastSyncTime);
-
-        runInOrgContext(() -> {
-            new NamedParameterJdbcTemplate(jdbcTemplate).update(sql, params);
-            return NullObject.instance();
-        }, jdbcTemplate);
-    }
-
 
     private String wrapInQuotes(String parameter) {
         return parameter == null ? "null" : "\"" + parameter + "\"";
